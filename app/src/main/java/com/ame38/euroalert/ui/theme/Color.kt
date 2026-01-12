499ce0f41c67d82730d20fdaaa8dfe33d5fad291
ame38 <8823178+ame38@users.noreply.github.com>
2026-01-12 11:02:37 +0000

    compose colors + type scale

---
+++ app/src/main/java/com/ame38/euroalert/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.ame38.euroalert.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val AlertOrange = Color(0xFFE8752B)
+val AlertRed = Color(0xFFC62828)
+val AlertYellow = Color(0xFFF2B705)
+val SurfaceDark = Color(0xFF1B1B1B)
+val SurfaceLight = Color(0xFFFFFBF7)
